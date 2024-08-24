b2642d33f2616ac2a4a87c2ece5ded33d6c6fae2
artem <artemmakarenko12@gmail.com>
2024-08-24 16:18:26 +0300

    Final commit

---
+++ src/main/java/com/example/persistence/repository/token/TokenRepository.java
@@ -0,0 +1,8 @@
+package com.example.persistence.repository.token;
+
+import com.example.persistence.entity.token.Token;
+import com.example.persistence.repository.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends BaseRepository<Token> { }
